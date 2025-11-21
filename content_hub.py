9a44c9da17d7486d4406c1e136acecc8fbb3f85c
Matthew Davis <matthewddavis18@gmail.com>
2025-11-21 11:12:38 -0800

    Began working on sign in and create account pages

--- content_hub.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
-
-app = Flask(__name__)
-bootstrap = Bootstrap5(app)
-
-@app.route('/')
-def sign_in():
-    return render_template('sign_template.html')
