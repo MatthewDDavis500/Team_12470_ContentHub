165011bee6c22d56ee5a2b34e425a33f3382de44
Matthew Davis <matthewddavis18@gmail.com>
2025-11-21 12:33:39 -0800

    Sign in and create account templates functional, and form validation is functional

--- main_hub.py
+++ main_hub.py
@@ -1,9 +1,8 @@
 from pprint import pprint
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, validators
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
@@ -12,16 +11,43 @@ bootstrap = Bootstrap5(app)
 class SignInForm(FlaskForm):
     username = StringField(
         'Username: ', 
-        validators=[DataRequired()]
+        validators=[validators.DataRequired()]
     )
 
     password = StringField(
         'Password: ', 
-        validators=[DataRequired()]
+        validators=[validators.DataRequired()]
     )
 
 def attempt_sign_in(username, password):
+    print('Attempting Sign-In!')
     print(f'Username: {username}  Password: {password}')
+
+class AccountCreationForm(FlaskForm):
+    fname = StringField(
+        'First Name: ', 
+        validators=[validators.DataRequired()]
+    )
+
+    lname = StringField(
+        'Last Name: ', 
+        validators=[validators.DataRequired()]
+    )
+
+    username = StringField(
+        'Username: ', 
+        validators=[validators.DataRequired()]
+    )
+
+    password = PasswordField(
+        'Password: ', 
+        validators=[validators.DataRequired(), validators.Length(min=6)]
+    )
+
+def attempt_account_creation(fname, lname, username, password):
+    print('Attempting Account Creation!')
+    print(f'{fname} {lname}\'s account: ')
+    print(f'    Username: {username}  Password: {password}')
 
 @app.route('/', methods=('GET', 'POST'))
 def sign_in():
@@ -31,7 +57,14 @@ def sign_in():
         return redirect('/content_hub')
     return render_template('sign_in_template.html', form=form)
 
-@app.route('/create_an_account')
+@app.route('/create_an_account', methods=('GET', 'POST'))
 def create_account():
-    return render_template('create_account_template.html')
+    form = AccountCreationForm()
+    if form.validate_on_submit():
+        attempt_account_creation(form.fname.data, form.lname.data, form.username.data, form.password.data)
+        return redirect('/content_hub')
+    return render_template('create_account_template.html', form=form)
 
+@app.route('/content_hub')
+def content_hub():
+    return render_template('test.html')
